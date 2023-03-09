7b41d2cd7572f61a2aab05a04efa490d4c0b4ce7
AntonZenko <zenko_anton@ukr.net>
2023-03-09 22:09:08 +0200

    update header, links,responsible markup

---
+++ src/js/activeLink.js
@@ -0,0 +1,12 @@
+const links = document.querySelector('.nav__list');
+
+const activeLinkToggle = e => {
+  const activeLink = document.querySelector('.active');
+  if (activeLink === e.target || e.target === e.currentTarget) {
+    return;
+  }
+  activeLink.classList.remove('active');
+  e.target.classList.add('active');
+};
+
+links.addEventListener('click', activeLinkToggle);
